81c965f5f409f1c22cae49bb24350d1a8a804ad0
Renato de Oliveira Lucena <cpdrenato@gmail.com>
2020-06-30 10:13:05 -0300

    views

---
+++ resources/lang/pt-BR/core.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'add'     => 'Adicionar',
+    'remove'  => 'Remover',
+    'back'    => 'Voltar',
+    'close'   => 'Fechar',
+
+    'name'    => 'Nome',
+    'cpf'     => 'CPF',
+    'email'   => 'Email',
+    'phone'   => 'Telefone',
+
+    'plate'     => 'Placa',
+    'renavam'   => 'Renavam',
+    'brand'     => 'Marca',
+    'model'     => 'Modelo',
+    'color'     => 'Cor',
+    'year'      => 'Ano',
+    'owner'     => 'Proprietário',
+    'owner_id'  => 'Identificador do Proprietário',
+
+    'admin'                   => 'Administrador',
+    'client'                  => 'Cliente',
+    'clients'                 => 'Clientes',
+    'no_registered_customers' => 'Não há clientes registrado!',
+
+    'vehicles'                => 'Veículos',
+    'add_vehicle'             => 'Adicionar veículo',
+    'remover_vehicle'         => 'Remover veículo',
+    'manage_vehicles'         => 'Gerenciar veículos',
+    'no_registered_vehicles'  => 'Não há veículos registrado!',
+
+    'save_success'      => 'Salvo com sucesso!',
+    'save_fail'         => 'Ocorreu uma falha ao salvar :(',
+
+    'remove_success'    => 'Removido com sucesso!',
+    'remove_fail'       => 'Ocorreu uma falha ao remover :(',
+
+    'added'   => 'Adicionou',
+    'updated' => 'Modificou',
+    'deleted' => 'Removeu'
+];
